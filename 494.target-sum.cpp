4ad0bdfcc342f24d4d6ed61cdeb72fdc32f773fe
Hanting Zhang <hantingz@usc.edu>
2022-03-06 22:50:41 -0800

    finish 494 target sum

--- 494.target-sum.cpp
+++ 494.target-sum.cpp
@@ -14,25 +14,46 @@ private:
     int ans;
     vector<int> nums;
     int target;
+    int total;
+    int memo[21][2001];
 
 public:
     int findTargetSumWays(vector<int>& nums_, int target_) {
         ans = 0;
         nums = nums_;
         target = target_;
-        dfs(0, 0);
-        return ans;
+        total = 0;
+        for (auto& n : nums_)
+            total += n;
+        for (int i = 0; i < 21; ++i) {
+            for (int j = 0; j < 2001; ++j) {
+                memo[i][j] = -1;
+            }
+        }
+        return dfs(0, 0);
     }
 
-    void dfs(int sum, int i) {
+    int dfs(int sum, int i) {
         if (i == nums.size()) {
             if (sum == target)
-                ans++;
-            return;
+                return 1;
+            return 0;
+        }
+        int add, sub;
+        if (memo[i + 1][sum + nums[i] + total] == -1) {
+            add = dfs(sum + nums[i], i + 1);
+            memo[i + 1][sum + nums[i] + total] = add;
+        } else {
+            add = memo[i + 1][sum + nums[i] + total];
         }
 
-        dfs(sum + nums[i], i + 1);
-        dfs(sum - nums[i], i + 1);
+        if (memo[i + 1][sum - nums[i] + total] == -1) {
+            sub = dfs(sum - nums[i], i + 1);
+            memo[i + 1][sum - nums[i] + total] = sub;
+        } else {
+            sub = memo[i + 1][sum - nums[i] + total];
+        }
+        return add + sub;
     }
 };
 // @lc code=end
